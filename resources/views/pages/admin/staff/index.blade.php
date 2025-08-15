7c6d4987e7b6dfd34ec3034d4327f009d8fab06f
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-08-15 10:55:54 +0700

    add staff guru index

--- resources/views/pages/admin/staff/index.blade.php
+++ resources/views/pages/admin/staff/index.blade.php
@@ -3,14 +3,16 @@
 @section('title', 'Staff Yayasan')
 
 @section('content')
-    <x-cms_page title="Staff Yayasan" breadcrumb="Staff Yayasan" sub_breadcrumb="Daftar Anggota">
+    <x-cms_page :title="$title" breadcrumb1="Tentang Sekolah" :breadcrumb2="$title" :breadcrumb3="$breadcrumb3">
 
         <!-- Isi konten lainnya -->
         <div class="flex flex-row justify-between">
-            <p>Data Pimpinan</p>
-            <a href="#" class="bg-blue-700 hover:bg-blue-800 text-white py-3 px-6 rounded-2xl text-sm">
-                <i class="fa-regular fa-plus-square text-xl"></i>
-                Tambah Data</a>
+            <p>Data {{ $title }}</p>
+            <a href="#"
+                class="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white py-2 px-5 rounded-lg text-sm shadow-md transition-all duration-200">
+                <i class="fa-regular fa-plus text-base"></i>
+                <span class="font-semibold">Tambah Data</span>
+            </a>
         </div>
 
         <table class="table-auto w-full mt-4 text-sm">
@@ -19,35 +21,36 @@
                     <th class="border px-4 py-2">No</th>
                     <th class="border px-4 py-2">Nama</th>
                     <th class="border px-4 py-2">Jabatan</th>
-                    <th class="border px-4 py-2">Stat
-                        // Menaus Publish</th>
+                    <th class="border px-4 py-2">Status Publish</th>
                     <th class="border px-4 py-2">Aksi</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td class="border px-4 py-2 text-center">1</td>
-                    <td class="border px-4 py-2">Muhammad Rasyid</td>
-                    <td class="border px-4 py-2">Pimpinan</td>
-                    <td class="border px-4 py-2 flex justify-center">
-                        <div class="flex bg-green-100 px-4 py-2 text-center w-fit rounded-lg items-center justify-center">
-                            <div class="w-2 h-2 bg-green-500 rounded-full"></div>
-                            <p class=" ms-2 text-green-800 rounded-md text-sm">Dipublish</p>
-                        </div>
-                    </td>
-                    <td class="border px-4 py-2">
-                        <div class="flex gap-2 justify-center">
-                            <a href="#"
-                                class="border border-blue-700 hover:border-blue-800 text-blue-700 hover:text-blue-800 py-2 px-3 rounded-2xl text-md">
-                                <i class="fa-regular fa-pen-to-square"></i>
-                            </a>
-                            <a href="#"
-                                class="border border-red-700 hover:border-red-800 text-red-700 hover:text-red-800 py-2 px-3 rounded-2xl text-md">
-                                <i class="fa-regular fa-trash-can"></i>
-                            </a>
-                        </div>
-                    </td>
-                </tr>
+                @forelse ($staffData as $item)
+                    <tr>
+                        <td class="border px-4 py-2 align-middle text-center">{{ $loop->iteration }}</td>
+                        <td class="border px-4 py-2 align-middle">{{ $item['nama'] }}</td>
+                        <td class="border px-4 py-2 align-middle">{{ $item['jabatan'] }}</td>
+                        <td class="border px-4 py-2 align-middle text-center flex justify-center">
+                            <x-status-publish :status="$item['status']" />
+                        </td>
+                        <td class="border px-4 py-2 align-middle">
+                            <div class="flex gap-2 justify-center">
+                                <a href="#" class="detail-btn-table"><i class="text-sm fa-regular fa-eye"></i></a>
+                                <a href="#" class="edit-btn-table"><i
+                                        class="text-sm fa-regular fa-pen-to-square"></i></a>
+                                <a href="#" class="delete-btn-table"><i
+                                        class="text-sm fa-regular fa-trash-can"></i></a>
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="5" class="text-center border p-4 text-gray-500">
+                            Belum ada data.
+                        </td>
+                    </tr>
+                @endforelse
             </tbody>
         </table>
     </x-cms_page>
