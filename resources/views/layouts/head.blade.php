c746fb88a433c62e8a8b053f22ccc0440f45a80e
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-08-27 20:56:25 +0700

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -15,4 +15,5 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('images/dimsa_blue.png') }}">
     @stack('styles')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </head>
