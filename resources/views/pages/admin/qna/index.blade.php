3d6a1ed6cbbb1e0b5ab2fcafaf8e564bf5bbd3bb
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-08-28 13:24:20 +0700

    add qna getAll

--- resources/views/pages/admin/qna/index.blade.php
+++ resources/views/pages/admin/qna/index.blade.php
@@ -4,5 +4,81 @@
 
 @section('content')
     <x-cms_page title="Manajemen QNA" breadcrumb1="Admin" breadcrumb2="Informasi" breadcrumb3="QNA">
+
+        <div class="w-full flex flex-row justify-end mb-5">
+            <a href="#"
+                class="w-fit flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white py-2 px-5 rounded-lg text-sm shadow-md transition-all duration-200">
+                <i class="fa-regular fa-plus text-base"></i>
+                <span class="font-semibold">Tambah Data</span>
+            </a>
+        </div>
+
+        <div class="overflow-x-auto">
+            <table class="min-w-full" id="qna-datatable">
+                <thead>
+                    <tr>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            No.</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Pertanyaan</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Jawaban</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Status</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+
     </x-cms_page>
 @endsection
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            function loadQna() {
+                console.log(1);
+
+                $("#qna-datatable").DataTable({
+                    processing: true,
+                    serverSide: true,
+                    autoWidth: false,
+                    responsive: true,
+                    ajax: "{{ url('admin/api/qna') }}",
+                    columns: [{
+                            data: 'DT_RowIndex',
+                            name: 'DT_RowIndex'
+                        },
+                        {
+                            data: 'question',
+                            name: 'question',
+                        },
+                        {
+                            data: 'answer',
+                            name: 'answer'
+                        },
+                        {
+                            data: 'status',
+                            name: 'status'
+                        },
+                        {
+                            data: 'aksi',
+                            name: 'aksi',
+                            orderable: false,
+                            searchable: false
+                        }
+                    ]
+                })
+            }
+            loadQna();
+        });
+    </script>
+@endpush
