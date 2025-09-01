3817deef496e882edb32e76317001ae3bafe9a11
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-09-02 01:36:16 +0700

    add get ektrakurikuler

--- resources/views/pages/admin/ekstrakurikuler/index.blade.php
+++ resources/views/pages/admin/ekstrakurikuler/index.blade.php
@@ -4,6 +4,85 @@
 
 @section('content')
     <x-cms_page title="Manajemen Extrakurikuler" breadcrumb1="Admin" breadcrumb2="Extrakurikuler" breadcrumb3="Extrakurikuler">
+        <div class="w-full flex flex-row justify-end mb-5">
+            <a href="{{ route('admin.ekstrakurikuler.create') }}"
+                class="w-fit flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white py-2 px-5 rounded-lg text-sm shadow-md transition-all duration-200">
+                <i class="fa-regular fa-plus text-base"></i>
+                <span class="font-semibold">Tambah Data</span>
+            </a>
+        </div>
 
+        <div class="overflow-x-auto">
+            <table class="min-w-full" id="ekstrakurikuler-datatable">
+                <thead>
+                    <tr>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            No.</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Thumbnail</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Judul</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Status</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
     </x-cms_page>
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            function loadEkstrakurikuler() {
+                $("#ekstrakurikuler-datatable").DataTable({
+                    processing: true,
+                    serverSide: true,
+                    autoWidth: false,
+                    responsive: true,
+                    dom: '<"md:flex md:justify-between items-center mb-4"lf>t<"md:flex md:justify-between items-center mt-4"ip>',
+                    ajax: "{{ url('/admin/api/ekstrakulikuler') }}",
+                    columns: [{
+                            data: 'DT_RowIndex',
+                            name: 'DT_RowIndex',
+                            orderable: false, // Biasanya kolom nomor tidak perlu diurutkan
+                            searchable: false
+                        },
+                        {
+                            data: 'thumbnail',
+                            name: 'thumbnail',
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: 'judul',
+                            name: 'judul'
+                        },
+                        {
+                            data: 'status',
+                            name: 'status',
+                        },
+                        {
+                            data: 'aksi',
+                            name: 'aksi',
+                            orderable: false,
+                            searchable: false,
+                            className: 'text-center'
+                        }
+                    ]
+                });
+            }
+
+            loadEkstrakurikuler();
+        });
+    </script>
+@endpush
