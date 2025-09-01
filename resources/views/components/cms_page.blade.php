242c82df69f82c852c07e3fd0f516e35bbb01de5
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-09-02 00:33:19 +0700

    Update cms_page.blade.php

--- resources/views/components/cms_page.blade.php
+++ resources/views/components/cms_page.blade.php
@@ -7,7 +7,8 @@
     'breadcrumb3' => 'Default Sub Breadcrumb',
 ])
 
-<div class="container mx-auto p-2">
+{{-- PERBAIKAN: Menghapus div "container" dari sini untuk menghindari container ganda --}}
+<div>
     <div class="flex flex-row justify-between items-end">
         <div class="flex flex-col">
             <h1 class="text-3xl text-gray-900">{{ $title ?? 'Default Title' }}</h1>
@@ -19,12 +20,12 @@
         </div>
 
         {{-- Slot untuk Aksi di Kanan (misalnya, tombol tab) --}}
-        @if (isset($action))
+        {{-- PERBAIKAN: Mengubah $action menjadi $actions agar sesuai dengan pemanggilan slot --}}
+        @if (isset($actions))
             <div class="mt-4 md:mt-0 md:ml-4">
-                {{ $action }}
+                {{ $actions }}
             </div>
         @endif
-
 
     </div>
 
