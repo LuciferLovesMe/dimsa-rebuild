24cddcbdc41d272b3795a365395799153369d998
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-09-02 10:00:05 +0700

    add get agenda

--- resources/views/pages/admin/agenda/index.blade.php
+++ resources/views/pages/admin/agenda/index.blade.php
@@ -4,7 +4,93 @@
 @section('content')
 
     <x-cms_page title="Manajemen Agenda" breadcrumb1="Admin" breadcrumb2="Informasi" breadcrumb3="Manajemen Agenda">
-
+        <div class="w-full flex flex-row justify-end mb-5">
+            <a href="{{ route('admin.agenda.create') }}"
+                class="w-fit flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white py-2 px-5 rounded-lg text-sm shadow-md transition-all duration-200">
+                <i class="fa-regular fa-plus text-base"></i>
+                <span class="font-semibold">Tambah Data</span>
+            </a>
+        </div>
+        <div class="overflow-x-auto">
+            <table class="min-w-full" id="agenda-datatable">
+                <thead>
+                    <tr>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            No.</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Cover</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Nama</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Tanggal</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Alamat</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Status</th>
+                        <th
+                            class="px-6 py-3 border-b-2 border-gray-300 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
     </x-cms_page>
-
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            function loadAgenda() {
+                $("#agenda-datatable").DataTable({
+                    processing: true,
+                    serverSide: true,
+                    autoWidth: false,
+                    responsive: true,
+                    dom: '<"md:flex md:justify-between items-center mb-4"lf>t<"md:flex md:justify-between items-center mt-4"ip>',
+                    ajax: "{{ url('admin/api/agenda') }}",
+                    columns: [{
+                            data: 'DT_RowIndex',
+                            name: 'DT_RowIndex'
+                        },
+                        {
+                            data: 'image',
+                            name: 'image',
+                        },
+                        {
+                            data: 'nama',
+                            name: 'nama'
+                        },
+                        {
+                            data: 'datetime',
+                            name: 'datetime'
+                        },
+                        {
+                            data: 'alamat',
+                            name: 'alamat'
+                        },
+                        {
+                            data: 'status',
+                            name: 'status'
+                        },
+                        {
+                            data: 'aksi',
+                            name: 'aksi',
+                            orderable: false,
+                            searchable: false
+                        }
+                    ]
+                })
+            }
+
+            loadAgenda();
+        });
+    </script>
+@endpush
