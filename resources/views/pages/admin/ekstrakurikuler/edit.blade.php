c804d7cb1bdbe84d456a98fec6d0e51e89bb2dd1
strlele <widyapuspitasari423@gmail.com>
2025-09-04 14:56:34 +0700

    update edit ekstrakurikuler

---
+++ resources/views/pages/admin/ekstrakurikuler/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.form-cms')
+
+@section('title', 'Edit Data Ekstrakurikuler')
+
+@section('backUrl', route('admin.ekstrakurikuler.index'))
+
+@section('pageTitle', 'Edit Ekstrakurikuler')
+
+@section('formContent')
+    <form action="#" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-8">
+
+            <div class="lg:col-span-2 space-y-6">
+
+                <x-input.textarea name="judul" label="Judul Ekstrakurikuler" placeholder="Judul Ekstrakurikuler" :required="true" />
+
+                <x-input.link name="link_dokumen" label="Link Dokumen" placeholder="Tulis link" :required="true" />
+
+                <x-input.publish-checkbox />
+
+            </div>
+
+            <div class="lg:col-span-2 flex flex-col justify-between">
+                <x-input.image-uploader name="image" />
+                <x-button.save text="Perbarui Data" />
+            </div>
+
+        </div>
+    </form>
+@endsection
