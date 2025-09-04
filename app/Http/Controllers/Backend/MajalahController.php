559b475f3b405b6d2cc5f1328f53653d81a92ed6
LuciferLovesMe <68785383+LuciferLovesMe@users.noreply.github.com>
2025-09-04 15:01:46 +0700

    Update MajalahController.php

--- app/Http/Controllers/Backend/MajalahController.php
+++ app/Http/Controllers/Backend/MajalahController.php
@@ -44,7 +44,7 @@ class MajalahController extends Controller
                 })
                 ->addColumn('aksi', function ($item) {
                     $actionButton = new ActionButton(
-                        '#', '#', '#'
+                        '#', url('/admin/majalah/edit') . '?id=' . $item->id, '#'
                     );
                     return $actionButton->render()->with($actionButton->data());
                 })
