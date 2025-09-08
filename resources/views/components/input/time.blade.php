93e1bbcecfbecce4f6e755481f38a62df90dde0e
Lutfi Hakim <lutfihakim1606@gmail.com>
2025-09-08 08:42:59 +0700

    add time picker component

---
+++ resources/views/components/input/time.blade.php
@@ -0,0 +1,14 @@
+@props(['name', 'label', 'value' => '', 'required' => false])
+
+<div>
+    <label for="{{ $name }}" class="block text-sm font-medium text-gray-700">
+        {{ $label }}
+        @if ($required)
+            <span class="text-red-600">*</span>
+        @endif
+    </label>
+    <input type="time" name="{{ $name }}" id="{{ $name }}"
+        class="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        value="{{ old($name, $value) }}">
+    <p id="error-{{ $name }}" class="mt-1 text-xs text-red-600"></p>
+</div>
