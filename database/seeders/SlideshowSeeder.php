2b5fa62e05c4a8e1094242ffb8d3bdaa7cd3c804
achfairuz <fairf717@gmail.com>
2025-09-12 09:04:23 +0700

    update seeder slideshow

--- database/seeders/SlideshowSeeder.php
+++ database/seeders/SlideshowSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\slideshow;
+use App\Models\Slideshow;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +13,6 @@ class SlideshowSeeder extends Seeder
      */
     public function run(): void
     {
-        slideshow::factory()->count(3)->create();
+        Slideshow::factory()->count(3)->create();
     }
 }
